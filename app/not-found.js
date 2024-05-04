dd18ae318c37168749f009fd0cb91198b77ba4e8
Adhham Safwan <msmadhhamg@gmail.com>
2024-05-04 23:27:05 +0530

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -1,3 +1,5 @@
+import {Link} from 'next/link'
+
 export default function NotFound() {
     return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -25,12 +27,13 @@ export default function NotFound() {
           The page you{"'"}re looking for doesn{"'"}t exist.
         </p>
         <div className="flex justify-center">
-          <a
+          <Link
             href="/"
             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+scroll={false}
           >
             Go Home
-          </a>
+          </Link>
         </div>
       </div>
     </div>

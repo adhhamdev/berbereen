e3897411c3bdf79a4abda0548557d616e9a15380
adhhamdev <msmadhhamg@gmail.com>
2024-05-05 16:59:40 +0530

    Refactor CardOptions component for improved accessibility and button behavior

--- components/card-options.js
+++ components/card-options.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Transition from "./transition";
 import { AnimatePresence } from "framer-motion";
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 
 export default function CardOptions() {
   const [optionsVisible, setOptionsVisible] = useState(false);
@@ -20,11 +21,11 @@ export default function CardOptions() {
             className="absolute right-2 p-2 bg-white shadow-lg z-10 rounded-lg"
           >
             <div>
-              <button className="flex items-center py-2 px-4 hover:bg-slate-200 rounded-md">
-                Edit
+              <button className="size-full flex items-center px-4 py-2 hover:bg-slate-200 rounded-md">
+                <PencilSquareIcon className="size-5 mr-2" /> Edit
               </button>
-              <button className="flex items-center py-2 px-4 hover:bg-slate-200 rounded-md">
-                Delete
+              <button className="size-full flex items-center px-4 py-2 hover:bg-slate-200 rounded-md">
+                <TrashIcon className="size-5 mr-2" /> Delete
               </button>
             </div>
           </Transition>

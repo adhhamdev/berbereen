7341b20180379136fb40999ec6488f5a33e67434
adhhamdev <msmadhhamg@gmail.com>
2024-05-06 19:08:40 +0530

    feat: Add profile picture to user account page
    The code changes include adding a profile picture to the user account page. This is achieved by importing the `Image` component from Next.js and using it to display the user's profile picture fetched from the `randomuser.me` API. The profile picture is displayed in a circular shape with a border, and the user's name and email are also shown.
    
    The commit message suggests adding a profile picture to the user account page, which accurately describes the purpose of the code changes.

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await getLoggedInUser();
+  console.log(user)
 
   if (!user) redirect("/login");
   const listData = [

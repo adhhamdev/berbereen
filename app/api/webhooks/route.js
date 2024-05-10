aa9455ce302d3e6f312c20a5ea65671d13bffad4
adhhamdev <msmadhhamg@gmail.com>
2024-05-10 19:32:54 +0530

    Refactor webhook route to use POST method and update logging

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export const POST = (req) => {
-    const {nextUrl} = req;
-    console.log(nextUrl.options)
+    const headers = req.headers;
+    console.log(headers)
     return NextResponse.json({})
 }

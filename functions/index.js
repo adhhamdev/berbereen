10804a3864ed2fa78a3d606dadc6a8b91a67c3df
adhhamdev <msmadhhamg@gmail.com>
2024-05-10 22:28:49 +0530

    chore: Update import path for createDatabaseClient and log request data in userEventHandler

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 import { createDatabaseClient } from "./actions.js";
 
 const userEventHandler = async ({ req, res, log, error }) => {
-  const data = await req.json();
+  const data = JSON.stringify(req.body)
+  const headers = JSON.stringify(req.headers);
   log(data)
+  log(headers)
   // const { database } = await createDatabaseClient();
   // database.createDocument();
   return res.empty();

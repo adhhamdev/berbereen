6bc1fd4fb3637f358cd2f3fc11af722b25438caf
adhhamdev <msmadhhamg@gmail.com>
2024-05-10 21:59:25 +0530

    chore: Update node-appwrite import to use double quotes

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,11 @@
-import { Client } from 'node-appwrite';
+import { Client } from "node-appwrite";
 const userEventHandler = async ({ req, res, log, error }) => {
   const client = new Client()
-     .setEndpoint('https://cloud.appwrite.io/v1')
-     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-     .setKey(process.env.APPWRITE_API_KEY);
-     log('Apwrite function working...!');
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+  log("Apwrite function working...!");
+  return res.empty();
 };
 
 export default userEventHandler;

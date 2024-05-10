b66fcf8a9d6957b728184dde5d5d57803d93fdc1
adhhamdev <msmadhhamg@gmail.com>
2024-05-10 18:25:28 +0530

    feat: Refactor webhook route to use POST method and remove unnecessary logging

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export const POST = (req) => {
-    const {method, body, url, nextUrl, cookies} = req;
-    console.log(method, body, url, nextUrl, cookies)
+    const {nextUrl} = req;
+    console.log(nextUrl)
     return NextResponse.json({"message": "Webhook request recieved!"})
 }

9f1710c4da0943de9663823efcfe4e000f283451
adhhamdev <msmadhhamg@gmail.com>
2024-05-11 09:33:32 +0530

    Refactor redirect logic in page.js and actions.js

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -5,7 +5,6 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const user = await getLoggedInUser();
   if (!user) redirect("/signup");
-  redirect("/");
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3">
       <div className="bg-slate-600"></div>

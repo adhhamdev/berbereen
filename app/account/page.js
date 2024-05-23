d9e8cab1ef5d0181548451085746611384d7a3b4
adhhamdev <msmadhhamg@gmail.com>
2024-05-23 20:52:01 +0530

    refactor: Update page header to include user email in account page

--- app/account/page.js
+++ app/account/page.js
@@ -2,7 +2,7 @@ import { getLoggedInUser } from "@/lib/server/appwrite";
 import { deleteUser, getAvatar } from "@/lib/server/actions";
 import Image from "next/image";
 import Transition from "../../components/transition";
-import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+import { ArrowLeftIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 
 export const metadata = {
@@ -24,7 +24,7 @@ export default async function Page() {
 
   return (
     <div>
-      <header className="flex items-center justify-between border-b px-4">
+      <header className="flex items-center justify-between border-b lg:px-4">
         <div className="flex items-center">
           <Link
             href="/"
@@ -61,19 +61,7 @@ export default async function Page() {
                 {user?.name}
               </h3>
               <div className="inline-flex text-gray-700 dark:text-gray-300 items-center">
-                <svg
-                  className="h-5 w-5 text-gray-400 dark:text-gray-600 mr-1"
-                  fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path
-                    className=""
-                    d="M5.64 16.36a9 9 0 1 1 12.72 0l-5.65 5.66a1 1 0 0 1-1.42 0l-5.65-5.66zm11.31-1.41a7 7 0 1 0-9.9 0L12 19.9l4.95-4.95zM12 14a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
-                  />
-                </svg>
+                <EnvelopeIcon className="size-4 mr-2" />
                 {user?.email}
               </div>
             </div>

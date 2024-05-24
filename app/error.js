eb5002022ae623db30d11ac00f28f2c86eac5b5b
adhhamdev <msmadhhamg@gmail.com>
2024-05-24 14:58:23 +0530

    refactor: Improve error handling and messaging in app/error.js and lib/server/actions.js

--- app/error.js
+++ app/error.js
@@ -14,7 +14,13 @@ export default function Error({ error, reset }) {
     <div className="flex flex-col items-center justify-center h-screen bg-white">
       <div className="p-8 rounded-xl shadow-md">
         <h2 className="text-2xl font-semibold text-black mb-4">
-          {error.message}
+          {error.statusCode === 404 ? (
+            <span>Oops, the thing you&apos;re looking for doesn&apos;t exist.</span>
+          ) : error.statusCode === 500 ? (
+            <span>Uh oh, something went wrong on our end.</span>
+          ) : (
+            <span>{error.message || "An unexpected error occurred."}</span>
+          )}
         </h2>
         <button
           className="bg-black hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"

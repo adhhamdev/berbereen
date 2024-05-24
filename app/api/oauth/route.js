99a21d1263ef1bef416e4f9c65cd5e8952d9abf8
adhhamdev <msmadhhamg@gmail.com>
2024-05-24 16:01:33 +0530

    refactor: Add logging for userId and secret in OAuth route

--- app/api/oauth/route.js
+++ app/api/oauth/route.js
@@ -8,6 +8,7 @@ export async function GET(request) {
   try {
     const userId = request.nextUrl.searchParams.get("userId");
     const secret = request.nextUrl.searchParams.get("secret");
+    console.log(userId, secret)
 
     const { account } = await createAdminClient();
     const session = await account.createSession(userId, secret);

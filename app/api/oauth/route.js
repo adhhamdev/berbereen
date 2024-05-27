51a84de93503dc49960dd40d0aa361632e84a6e5
adhhamdev <msmadhhamg@gmail.com>
2024-05-27 21:41:28 +0530

    refactor: Update user session handling and avatar upload in OAuth and webhooks routes

--- app/api/oauth/route.js
+++ app/api/oauth/route.js
@@ -14,7 +14,7 @@ export async function GET(request) {
   const userId = request.nextUrl.searchParams.get("userId");
   const secret = request.nextUrl.searchParams.get("secret");
 
-  try {
+  // try {
     const { account } = await createAdminClient();
     const session = await account.createSession(userId, secret);
 
@@ -25,29 +25,29 @@ export async function GET(request) {
       secure: true,
     });
 
-    const user = await getLoggedInUser();
-    const { avatars } = await createAvatarsClient();
-    const { databases } = await createDatabasesClient();
-    const initialsAvatar = await avatars.getInitials(user.name);
-    const iconBuffer = Buffer.from(initialsAvatar, "base64");
-    const file = InputFile.fromBuffer(iconBuffer, "avatar");
-    const uploadedFile = await storage.createFile("primary", ID.unique(), file);
-    const updatedUserDoc = await databases.updateDocument(
-      "primary",
-      "user",
-      $id,
-      { avatar: uploadedFile.$id }
-    );
-    console.log(updatedUserDoc);
+  //   const user = await getLoggedInUser();
+  //   const { avatars } = await createAvatarsClient();
+  //   const { databases } = await createDatabasesClient();
+  //   const initialsAvatar = await avatars.getInitials(user.name);
+  //   const iconBuffer = Buffer.from(initialsAvatar, "base64");
+  //   const file = InputFile.fromBuffer(iconBuffer, "avatar");
+  //   const uploadedFile = await storage.createFile("primary", ID.unique(), file);
+  //   const updatedUserDoc = await databases.updateDocument(
+  //     "primary",
+  //     "user",
+  //     $id,
+  //     { avatar: uploadedFile.$id }
+  //   );
+  //   console.log(updatedUserDoc);
 
     return NextResponse.redirect(`${request.nextUrl.origin}/`);
-  } catch (error) {
-    console.error(error);
-    return new NextResponse(
-      "An error occurred. Please try again later or contact support if the issue persists.",
-      {
-        status: 500,
-      }
-    );
-  }
+  // } catch (error) {
+  //   console.error(error);
+  //   return new NextResponse(
+  //     "An error occurred. Please try again later or contact support if the issue persists.",
+  //     {
+  //       status: 500,
+  //     }
+  //   );
+  // }
 }

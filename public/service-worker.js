2f6cd3fe45a1558114f93a93e9a05f73e86814d7
adhhamdev <msmadhhamg@gmail.com>
2024-05-29 18:03:04 +0530

    add sw

---
+++ public/service-worker.js
@@ -0,0 +1,10 @@
+function registerServiceWorker() {
+    if (typeof window !== "undefined") {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("/sw.js").then((registration) => {
+          console.log("Service Worker registration successful:", registration);
+        });
+      }
+    }
+  }
+  

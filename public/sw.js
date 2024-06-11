eb1c9d55fd72735b0f802c8cc33766c961d1324f
adhhamdev <msmadhhamg@gmail.com>
2024-06-12 02:19:24 +0530

    refactor: Improve service worker caching and response cloning

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,14 @@
 const cacheName = "v1";
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", event => {
   event.respondWith(
     event.request.mode === "navigate" ||
-      (event.request.method === "GET" &&
-        event.request.headers.get("accept").includes("text/css"))
+    (event.request.method === "GET" &&
+      event.request.headers.get("accept").includes("text/css"))
       ? fetch(event.request)
-          .catch(function (err) {
-            return caches.match(event.request);
-          })
-          .then(function (res) {
-            return caches.open(cacheName).then(function (cache) {
+          .catch(err => caches.match(event.request))
+          .then(res => {
+            return caches.open(cacheName).then(cache => {
               cache.put(event.request.url, res.clone());
               return res;
             });
